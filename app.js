9a37d19a2b2e8579486d0c3831325c3ae8eee5ca
Praneeth Pedapati <praneethpedapati98@gmail.com>
2022-04-11 04:44:07 +0000

    API3

--- app.js
+++ app.js
@@ -32,9 +32,31 @@ initializeDBAndServer();
 
 app.get("/states/", async (request, response) => {
   let getStatesQuery = `
-        SELECT *
+        SELECT state_id AS stateId, state_name AS stateName, population
         FROM state
     `;
   let states = await db.all(getStatesQuery);
   response.send(states);
+});
+
+app.get("/states/:stateId/", async (request, response) => {
+  let { stateId } = request.params;
+  let getStateQuery = `
+        SELECT state_id AS stateId, state_name AS stateName, population
+        FROM state
+        WHERE state_id = ${stateId}
+    `;
+  let state = await db.get(getStateQuery);
+  response.send(state);
+});
+
+app.post("/districts/", async (request, response) => {
+  let districtDetails = request.body;
+  let { districtName, stateId, cases, cured, active, deaths } = districtDetails;
+  let postDistrictQuery = `
+        INSERT INTO district (district_name, state_id, cases, cured, active, deaths)
+        VALUES ('${districtName}', ${stateId}, ${cases}, ${cured}, ${active}, ${deaths});
+    `;
+  await db.run(postDistrictQuery);
+  response.send("District Successfully Added");
 });
